d985d5965e3ab6eef35d8fec23fddf52c5f4dfeb
MANIKANDAN <manidhiyamech07167@gmail.com>
2026-02-10 11:12:56 +0530

    Initial Commit

---
+++ src/Tab/App.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import * as teamsJs from "@microsoft/teams-js";
+
+import "./App.css";
+
+export default function App() {
+  const [content, setContent] = React.useState("");
+
+  React.useEffect(() => {
+    (async () => {
+      teamsJs.app.initialize().then(() => {
+        teamsJs.app.getContext().then((context: teamsJs.app.Context) => {
+          if (context?.app?.host?.name) {
+            setContent(`Your app is running in ${context.app.host.name}`);
+          }
+        });
+      });
+    })();
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>👋 Welcome</h1>
+
+      {content && (
+        <div className="result">
+          <pre>
+            <code>{content}</code>
+          </pre>
+        </div>
+      )}
+
+      <p>
+        For more information, please refer to the{" "}
+        <a
+          href="https://aka.ms/teams-ai-library-v2"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Microsoft Teams SDK 111
+        </a>
+        .
+      </p>
+    </div>
+  );
+}
